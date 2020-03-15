259e187749a468cbcb3f3392364c6364417329a6
Sergei Shulepov <s.pepyakin@gmail.com>
2020-03-15 16:57:38 +0100

    Commit Dockerfile

--- cross-build.sh
+++ cross-build.sh
@@ -2,6 +2,6 @@
 docker run --rm -it \
 	--env BINDGEN_EXTRA_CLANG_ARGS="--sysroot=/usr/arm-linux-gnueabihf -D__ARM_PCS_VFP -mfpu=vfp -mfloat-abi=hard" \
 	--env CARGO_HOME=/home/rust/src/cargo_home \
-	-v "$(pwd)":/home/rust/src 952d743ec425 \
+	-v "$(pwd)":/home/rust/src pepyakin/remarkable-oxide-cross-pi \
 	cargo build --target=armv7-unknown-linux-gnueabihf --release
 
